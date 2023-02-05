287e196bad9b9af37e43d1bde75abd7bf9dfbb4c
mianbao <mianbao@mianbaos-MBP.lan>
2023-02-04 19:48:55 -0500

    appointment bulit

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,9 +5,15 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+
+
 export default function Appointment(props) {
+    const { time, interview } = props
     return (
-        <article className="appointment"></article>
+        <article className="appointment">
+            <Header time={time} />
+            {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+        </article>
 
     );
 
